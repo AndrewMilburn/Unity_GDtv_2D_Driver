5d9bcc9ba1062748fa34fda07001347b8004727d
Andrew Milburn <sidbell.069@gmail.com>
2023-04-20 08:14:11 +0100

    L013 Done
    Getting rid of hard-coded numbers with Variables

--- Driver/Assets/Driver.cs
+++ Driver/Assets/Driver.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
+    float _fturnSpeed = 1;
+    float _fmoveSpeed = 0.01f;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -13,7 +16,7 @@ public class Driver : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(0, 0, 0.1f);
-        transform.Translate(0, 0.01f, 0);
+        transform.Rotate(0, 0, _fturnSpeed);
+        transform.Translate(0, _fmoveSpeed, 0);
     }
 }
