bf94b482093062f3006b820d9d882d2dc5b4f6dc
Andrew Milburn <sidbell.069@gmail.com>
2023-04-21 08:32:38 +0100

    End of L014
    Made the variables Serialized

--- Driver/Assets/Driver.cs
+++ Driver/Assets/Driver.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
-    float _fturnSpeed = 1;
-    float _fmoveSpeed = 0.01f;
+    [SerializeField] float _fturnSpeed = 1;
+    [SerializeField] float _fmoveSpeed = 0.01f;
     
     // Start is called before the first frame update
     void Start()
