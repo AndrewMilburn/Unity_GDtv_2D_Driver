2fbedf7889309c943ac48bd78650d59233e26b42
Andrew Milburn <sidbell.069@gmail.com>
2023-05-30 07:45:20 +0100

    L0024; Bools
    Added Package Logic to Delivery script

--- Driver/Assets/Delivery.cs
+++ Driver/Assets/Delivery.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
+    bool hasPackage;
+    
     void OnCollisionEnter2D(Collision2D collision)
     {
         Debug.Log("OUCH!!");
@@ -14,10 +16,12 @@ public class Delivery : MonoBehaviour
         if (collision.tag == "Package")
         {
             Debug.Log("Package picked up");
+            hasPackage = true;
         }
-        else if(collision.tag == "Customer")
+        else if(collision.tag == "Customer" && hasPackage)
         {
             Debug.Log("Package delivered");
+            hasPackage = false;
         }
         else
         {
